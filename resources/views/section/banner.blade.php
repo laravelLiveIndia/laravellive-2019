09fe1fe163ed946545195e6784e656faa3102f0d
sarthaksavvy <sarthak@bitfumes.com>
2018-10-12 12:17:19 +0530

    changed image

--- resources/views/section/banner.blade.php
+++ resources/views/section/banner.blade.php
@@ -1,28 +1,27 @@
 <section>
-    <div class="csi-banner csi-banner2" style="background-image: url('http://www.orangesmile.com/common/img_cities_original/bombay--2092174-11.jpg'); background-size: cover;">
-        <div style="background-color: black; opacity: 0.5">
-            <div class="csi-banner-style">
-                <div class="csi-inner">
-                    <div class="container">
-                        <div class="csi-banner-content">
-                            <h3 class="csi-subtitle">Conference On</h3>
-                            <h2 class="csi-title"><span id="csi-typed-string">User I</span><span class="typed-cursor">|</span></h2>
-                            <p class="date">02-05 December 2019, <span>Dhaka</span> </p>
-                        </div>
+    <div class="csi-banner csi-banner2">
+        <div class="csi-banner-style">
+            <div class="csi-inner">
+                <div class="container">
+                    <div class="csi-banner-content">
+                        <h3 class="csi-subtitle">Conference On</h3>
+                        <h2 class="csi-title"><span id="csi-typed-string">Laravel</span><span class="typed-cursor">|</span></h2>
+                        <p class="date">02-05 December 2019, <span>Dhaka</span> </p>
+                    </div>
 
-                        <div class="csi-countdown-area csi-countdown-area-left">
-                            <div class="csi-countdown-area-inner">
-                                <!-- Date Format :"Y/m/d" || For Example: 1017/10/5  -->
-                                <div id="csi-countdown" data-date="2019/12/15">
-                                    <span class="csi-days">434<i> Days</i></span> <span class="csi-hr">04 <i> Hour </i></span> <span class="csi-min">24<i> Minu </i></span> <span class="csi-sec">46 <i> Seco </i></span>
-                                </div>
+                    <div class="csi-countdown-area csi-countdown-area-left">
+                        <div class="csi-countdown-area-inner">
+                            <!-- Date Format :"Y/m/d" || For Example: 1017/10/5  -->
+                            <div id="csi-countdown" data-date="2019/12/15">
+                                <span class="csi-days">434<i> Days</i></span> <span class="csi-hr">04 <i> Hour </i></span>
+                                <span class="csi-min">24<i> Minu </i></span> <span class="csi-sec">46 <i> Seco </i></span>
                             </div>
                         </div>
                     </div>
-                    <!-- //.container -->
                 </div>
-                <!-- //.INNER -->
+                <!-- //.container -->
             </div>
+            <!-- //.INNER -->
         </div>
     </div>
 </section>
