8f436d388b41c7c4e31d3d7fa84748b54c2a2eeb
codehimanshu <himanshuagrawal1998@gmail.com>
2018-10-12 11:36:34 +0530

    change bg image to css and change timer

--- resources/views/section/banner.blade.php
+++ resources/views/section/banner.blade.php
@@ -1,5 +1,5 @@
 <section>
-    <div class="csi-banner csi-banner2" style="background-image: url('http://www.orangesmile.com/common/img_cities_original/bombay--2092174-11.jpg'); background-size: cover;">
+    <div class="csi-banner csi-banner2">
         <div style="background-color: black; opacity: 0.5">
             <div class="csi-banner-style">
                 <div class="csi-inner">
@@ -7,14 +7,14 @@
                         <div class="csi-banner-content">
                             <h3 class="csi-subtitle">Conference On</h3>
                             <h2 class="csi-title"><span id="csi-typed-string">User I</span><span class="typed-cursor">|</span></h2>
-                            <p class="date">02-05 December 2019, <span>Dhaka</span> </p>
+                            <p class="date">15 October 2018, <span>Mumbai</span> </p>
                         </div>
 
                         <div class="csi-countdown-area csi-countdown-area-left">
                             <div class="csi-countdown-area-inner">
                                 <!-- Date Format :"Y/m/d" || For Example: 1017/10/5  -->
-                                <div id="csi-countdown" data-date="2019/12/15">
-                                    <span class="csi-days">434<i> Days</i></span> <span class="csi-hr">04 <i> Hour </i></span> <span class="csi-min">24<i> Minu </i></span> <span class="csi-sec">46 <i> Seco </i></span>
+                                <div id="csi-countdown" data-date="2018/10/15 12:00">
+                                    <span class="csi-days">3<i> Days</i></span> <span class="csi-hr">00 <i> Hour </i></span> <span class="csi-min">28<i> Minu </i></span> <span class="csi-sec">30 <i> Seco </i></span>
                                 </div>
                             </div>
                         </div>
