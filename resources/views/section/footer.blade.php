f6e90698a0c20d89dd3483e8fa28103eb4b3040a
codehimanshu <himanshuagrawal1998@gmail.com>
2018-10-14 00:11:16 +0530

    tweek social icons

--- resources/views/section/footer.blade.php
+++ resources/views/section/footer.blade.php
@@ -9,13 +9,13 @@
                                 {{-- <div class="csi-footer-logo"> --}}
 
                                     <h2 style="color:white">Laravel<span style="color:#f55247">Live</span></h3>
-                                    {{-- </div> --}}
-                                </div>
-                                <div class="col-xs-12 col-sm-8">
-                                    <div class="csi-footer-single-area" style="text-align: end">
+                                {{-- </div> --}}
+                            </div>
+                            <div class="col-xs-12 col-sm-8">
+                                <div class="csi-footer-single-area" style="text-align: end">
                                         <!-- Begin Mailchimp Signup Form -->
                                         <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-
+                                                                             
                                         <div class="" id="mc_embed_signup">
                                             <center><h3 class="footer-title">Subscribe & Stay Updated</h3></center>
                                             <form  action="https://xyz.us15.list-manage.com/subscribe/post?u=a80aa20a6604dcef64860c596&amp;id=245bde01ba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate class="subscribe-form csi-subscribe-form" >
@@ -29,30 +29,55 @@
                                             </form>
                                         </div>
 
-                                        <div class="csi-footer-single">
+                                        <!--End mc_embed_signup-->
+                                    {{-- <div class="csi-footer-single">
+                                        <h3 class="footer-title">Venue Location </h3>
+                                        <h4 class="date">
+                                            18 - 21 December, 2019
+                                        </h4>
+                                        <address>
+                                            51 Francis Street, Darlinghurst <br>
+                                            NSW 2010, United States
+                                        </address>
+                                        <a class="map-link" href=""><i
+                                                class="fa fa-map-marker" aria-hidden="true"></i> View Map
+                                            location</a>
+                                    </div> --}}
+                                    <div class="csi-footer-single">
+                                        <center>
                                             <h3 class="footer-title">Social Connection</h3>
+                                            {{-- <p class="text">
+                                                Connect with us
+                                            </p> --}}
                                             <ul class="list-inline csi-social">
-                                                <li>
-                                                    <a href="https://www.facebook.com/laravellive/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://twitter.com/LaravelLiveIN"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="csi-copyright" style="text-align: center">
-                                    <p class="text">© 2018-19 LaravelLive</p>
+                                                <li><a href="https://www.facebook.com/laravellive/"><i
+                                                            class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                                <li><a href="https://twitter.com/LaravelLiveIN"><i
+                                                            class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                                        </ul>
+                                                    </div>
+                                        </center>
+                                    {{-- <div class="csi-footer-single">
+                                        <h3 class="footer-title">About</h3>
+                                        <p class="text">
+                                            Join the most productive Conference ever on Laravel in India. Not only your knowledge will increased but also make your social network bigger.
+                                            <br>
+                                            Get to know more advanced topics and enhance your Laravel skills to have more productivty.
+                                        </p>
+                                    </div> --}}
                                 </div>
                             </div>
                         </div>
-                        <!-- //.CONTAINER -->
+                        <div class="row">
+                            <div class="csi-copyright" style="text-align: center">
+                                <p class="text">© 2018-19 LaravelLive</p>
+                            </div>
+                        </div>
                     </div>
+                    <!-- //.CONTAINER -->
                 </div>
             </div>
-            <!-- //.footer Middle -->
         </div>
-    </footer>
+        <!-- //.footer Middle -->
+    </div>
+</footer>
