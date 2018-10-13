c19973ba308dcaff40f4b6dad5a5ecd50ccaba96
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2018-10-14 00:01:16 +0530

    align social icons

--- resources/views/section/footer.blade.php
+++ resources/views/section/footer.blade.php
@@ -7,17 +7,17 @@
                         <div class="row">
                             <div class="col-xs-12 col-sm-4">
                                 {{-- <div class="csi-footer-logo"> --}}
-                                    <h2 style="color:white">Laravel<span style="color:#f55247">Live</span></h3>
+                                    <h2 style="color:white">Laravel<span style="color:#f55247">Live</span></h2>
                                 {{-- </div> --}}
                             </div>
-                            <div class="col-xs-12 col-sm-8">
-                                <div class="csi-footer-single-area" style="text-align: end">
+                            <div class="col-xs-12 col-sm-4">
+                                <div class="csi-footer-single">
                                         <!-- Begin Mailchimp Signup Form -->
                                         <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
                                         <div id="mc_embed_signup">
                                             <form action="https://xyz.us15.list-manage.com/subscribe/post?u=a80aa20a6604dcef64860c596&amp;id=245bde01ba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                                                 <div id="mc_embed_signup_scroll">
-                                                    <label for="mce-EMAIL"><h3 class="text" style="color: white; margin-top: 0px;">Subscribe to get updates</h3></label>
+                                                    <label for="mce-EMAIL"><h3 class="text footer-title" style="color: white; margin-top: 0px;">Subscribe to get updates</h3></label>
                                                     <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required>
                                                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a80aa20a6604dcef64860c596_245bde01ba" tabindex="-1" value=""></div>
                                                     <br><br>
@@ -40,18 +40,6 @@
                                                 class="fa fa-map-marker" aria-hidden="true"></i> View Map
                                             location</a>
                                     </div> --}}
-                                    <div class="csi-footer-single">
-                                        <h3 class="footer-title">Social Connection</h3>
-                                        {{-- <p class="text">
-                                            Connect with us
-                                        </p> --}}
-                                        <ul class="list-inline csi-social">
-                                            <li><a href="https://www.facebook.com/laravellive/"><i
-                                                        class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                            <li><a href="https://twitter.com/LaravelLiveIN"><i
-                                                        class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                        </ul>
-                                    </div>
                                     {{-- <div class="csi-footer-single">
                                         <h3 class="footer-title">About</h3>
                                         <p class="text">
@@ -60,6 +48,18 @@
                                             Get to know more advanced topics and enhance your Laravel skills to have more productivty.
                                         </p>
                                     </div> --}}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-4 col-md-4 text-center">
+                                <div class="csi-footer-single">
+                                    <h3 class="footer-title">Social Connection</h3>
+
+                                    <ul class="list-inline csi-social">
+                                        <li style="padding-right: 10px;"><a href="https://www.facebook.com/laravellive/"><i
+                                                    class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://twitter.com/LaravelLiveIN"><i
+                                                    class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
