eb7cc63c323bd10a6fe46ba09bb345debedfb70c
Fahad Khan <fahadkhan1740@outlook.com>
2018-10-13 23:53:48 +0530

    removed navbar button

--- resources/views/section/header.blade.php
+++ resources/views/section/header.blade.php
@@ -8,12 +8,12 @@
                         <nav class="navbar navbar-default csi-navbar">
                             <div class="csicontainer">
                                 <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                    {{-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                                         <span class="sr-only">Toggle navigation</span>
                                         <span class="icon-bar" style="background-color:#f55247"></span>
                                         <span class="icon-bar" style="background-color:#f55247"></span>
                                         <span class="icon-bar" style="background-color:#f55247"></span>
-                                    </button>
+                                    </button> --}}
                                     <div class="csi-logo" style="margin-top:20px">
                                         <a href="/">
                                             {{-- <img src="{{ asset('images/logo.png') }}" alt="Logo"> --}}
