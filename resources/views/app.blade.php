1c77391c4184d15453923e7150c1395d7f265432
sarthaksavvy <sarthak@bitfumes.com>
2018-10-13 19:44:47 +0530

    changed font to oswald

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,9 +19,9 @@
 
     <!-- twitter card starts from here, if you don't need remove this section -->
     <meta name="twitter:card" content="summary">
-    <meta name="twitter:site" content="@yourtwitterusername">
-    <meta name="twitter:creator" content="@yourtwitterusername">
-    <meta name="twitter:url" content="http://yourdomain.com">
+    <meta name="twitter:site" content="@laravellivein">
+    <meta name="twitter:creator" content="@laravellivein">
+    <meta name="twitter:url" content="http://laravellive.in">
     <meta name="twitter:title" content="Your home page title, max 140 char"> <!-- maximum 140 char -->
     <meta name="twitter:description" content="Your site description, maximum 140 char "> <!-- maximum 140 char -->
     <meta name="twitter:image" content="assets/img/twittercardimg/twittercard-280-150.jpg">
@@ -30,9 +30,9 @@
 
     <!-- facebook open graph starts from here, if you don't need then delete open graph related  -->
     <meta property="og:title" content="Your home page title">
-    <meta property="og:url" content="http://your domain here.com">
+    <meta property="og:url" content="http://laravellive.in">
     <meta property="og:locale" content="en_US">
-    <meta property="og:site_name" content="Your site name here">
+    <meta property="og:site_name" content="Laravel Live">
     <!--meta property="fb:admins" content="" /-->
     <!-- use this if you have  -->
     <meta property="og:type" content="website">
@@ -44,21 +44,9 @@
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <!-- this icon shows in browser toolbar -->
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-    <!-- this icon shows in browser toolbar -->
-    {{-- <link rel="apple-touch-icon" sizes="57x57" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="http://httpcoder.com/demo/html/eventpro/view/assets/img/favicon/favicon-96x96.png"> --}}
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.png') }}">
     <link rel="manifest" href="/manifest.json">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 
     <!-- BOOTSTRAP CSS -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" media="all">
@@ -76,10 +64,10 @@
     <!-- MODERNIZER CSS  -->
     <script src="{{ asset('js/modernizr-2.8.3.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/demo-themeoptions.css') }}" media="all">
-    <script type="text/javascript" charset="UTF-8" src="{{ asset('js/common.js') }}"></script>
+    {{-- <script type="text/javascript" charset="UTF-8" src="{{ asset('js/common.js') }}"></script>
     <script type="text/javascript" charset="UTF-8" src="{{ asset('js/util.js') }}"></script>
-    <script type="text/javascript" charset="UTF-8" src="{{ asset('js/stats.js') }}"></script>
-    <script type="text/javascript" charset="UTF-8" src="{{ asset('js/AuthenticationService.Authenticate') }}"></script>
+    <script type="text/javascript" charset="UTF-8" src="{{ asset('js/stats.js') }}"></script> --}}
+    {{-- <script type="text/javascript" charset="UTF-8" src="{{ asset('js/AuthenticationService.Authenticate') }}"></script> --}}
 </head>
 
 <body class="home" data-gr-c-s-loaded="true" cz-shortcut-listen="true">
@@ -94,7 +82,7 @@
     <!-- SKILLS SCRIPT  -->
     <script src="{{ asset('js/jquery.validate.js') }}"></script>
     <!-- if load google maps then load this api, change api key as it may expire for limit cross as this is provided with any theme -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDzfNvH2kifJQ0PhBIyuuG-swdkW1NPQVE"></script>
+    {{-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDzfNvH2kifJQ0PhBIyuuG-swdkW1NPQVE"></script> --}}
     <!-- CUSTOM GOOGLE MAP -->
     <script src="{{ asset('js/jquery.googlemap.js') }}"></script>
     <!-- Owl Carousel  -->
