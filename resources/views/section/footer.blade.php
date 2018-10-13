c6797ae09cfb385c4b134213e85119f14ea27dd6
Fahad Khan <fahadkhan1740@outlook.com>
2018-10-14 00:30:01 +0530

    fixed footer

--- resources/views/section/footer.blade.php
+++ resources/views/section/footer.blade.php
@@ -12,58 +12,21 @@
                             </div>
                             <div class="col-xs-12 col-sm-4">
                                 <div class="csi-footer-single">
-                                        <!-- Begin Mailchimp Signup Form -->
-                                        <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+                                    <!-- Begin Mailchimp Signup Form -->
+                                    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
                                                                              
-                                        <div class="" id="mc_embed_signup">
-                                            <center><h3 class="footer-title">Subscribe & Stay Updated</h3></center>
-                                            <form  action="https://xyz.us15.list-manage.com/subscribe/post?u=a80aa20a6604dcef64860c596&amp;id=245bde01ba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate class="subscribe-form csi-subscribe-form" >
-                                                <div class="form-group form-group-email">
-                                                    <input type="email" id="subscribe" name="email" placeholder="Ex: themearth@gmail.com" class="form-control csi-input-form form-control" required="" />
-                                                </div>
-                                                <input type="hidden" name="b_a80aa20a6604dcef64860c596_245bde01ba" tabindex="-1" value="">
-                                                <div class="form-group form-group-submit">
-                                                    <button type="submit" name="subscribe" id="" class="csi-btn csi-submit"><span>Subscribe</span></button>
-                                                </div>
-                                            </form>
-                                        </div>
-
-                                        <!--End mc_embed_signup-->
-                                    {{-- <div class="csi-footer-single">
-                                        <h3 class="footer-title">Venue Location </h3>
-                                        <h4 class="date">
-                                            18 - 21 December, 2019
-                                        </h4>
-                                        <address>
-                                            51 Francis Street, Darlinghurst <br>
-                                            NSW 2010, United States
-                                        </address>
-                                        <a class="map-link" href=""><i
-                                                class="fa fa-map-marker" aria-hidden="true"></i> View Map
-                                            location</a>
-                                    </div> --}}
-                                    <div class="csi-footer-single">
-                                        <center>
-                                            <h3 class="footer-title">Social Connection</h3>
-                                            {{-- <p class="text">
-                                                Connect with us
-                                            </p> --}}
-                                            <ul class="list-inline csi-social">
-                                                <li><a href="https://www.facebook.com/laravellive/"><i
-                                                            class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                                <li><a href="https://twitter.com/LaravelLiveIN"><i
-                                                            class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                                        </ul>
-                                                    </div>
-                                        </center>
-                                    {{-- <div class="csi-footer-single">
-                                        <h3 class="footer-title">About</h3>
-                                        <p class="text">
-                                            Join the most productive Conference ever on Laravel in India. Not only your knowledge will increased but also make your social network bigger.
-                                            <br>
-                                            Get to know more advanced topics and enhance your Laravel skills to have more productivty.
-                                        </p>
-                                    </div> --}}
+                                    <div class="" id="mc_embed_signup">
+                                        <h3 class="footer-title" style="text-align: center">Subscribe & Stay Updated</h3>
+                                        <form  action="https://xyz.us15.list-manage.com/subscribe/post?u=a80aa20a6604dcef64860c596&amp;id=245bde01ba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate class="subscribe-form csi-subscribe-form" >
+                                            <div class="form-group form-group-email">
+                                                <input type="email" id="subscribe" name="email" placeholder="Ex: themearth@gmail.com" class="form-control csi-input-form form-control" required="" />
+                                            </div>
+                                            <input type="hidden" name="b_a80aa20a6604dcef64860c596_245bde01ba" tabindex="-1" value="">
+                                            <div class="form-group form-group-submit">
+                                                <button type="submit" name="subscribe" id="" class="csi-btn csi-submit"><span>Subscribe</span></button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-xs-12 col-sm-4 col-md-4 text-center">
