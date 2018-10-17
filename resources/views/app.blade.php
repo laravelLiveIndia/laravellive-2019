234dea35aacc8e02c8342df9eeb851d5e5c9fb75
Fahad Khan <fahadkhan1740@outlook.com>
2018-10-17 21:59:31 +0530

    added google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -68,6 +68,18 @@
     <script type="text/javascript" charset="UTF-8" src="{{ asset('js/util.js') }}"></script>
     <script type="text/javascript" charset="UTF-8" src="{{ asset('js/stats.js') }}"></script> --}}
     {{-- <script type="text/javascript" charset="UTF-8" src="{{ asset('js/AuthenticationService.Authenticate') }}"></script> --}}
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127639101-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-127639101-1');
+    </script>
+
+
 </head>
 
 <body class="home" data-gr-c-s-loaded="true" cz-shortcut-listen="true">
