b8a4ad2416812da9acf76698c465a353e623dc2f
Fahad Khan <fahad@Fahads-MacBook-Air.local>
2018-10-21 15:14:05 +0530

    added pune meetup link

--- resources/views/section/header.blade.php
+++ resources/views/section/header.blade.php
@@ -30,10 +30,10 @@
                                         <li class="dropdown">
                                             <a href="#">Community <span class="caret"></span></a>
                                             <ul style="margin-left: -30px;">
-                                                <li><a href="https://in.explara.com/e/laravelive-india-rajkot-meetup" target="_blank">Rajkot Meetup</a></li>
+                                                <li><a href="https://in.explara.com/e/laravelive-india-pune-meetup" target="_blank">Pune Meetup</a></li>
                                             </ul>
                                         </li>
-                                        <li><a class="csi-scroll" href=" https://www.papercall.io/laravellive-india">Call For Papers</a></li>
+                                        <li><a class="csi-scroll" href="https://www.papercall.io/laravellive-india">Call For Papers</a></li>
                                     </ul>
                                     <ul class="nav navbar-nav csi-nav" id="desktop_li">
                                         <li class="dropdown">
@@ -41,9 +41,9 @@
                                                 class="dropdown-toggle" data-toggle="dropdown" role="button"
                                                 aria-haspopup="true" aria-expanded="false">Community <span class="caret"></span></a>
                                             <ul class="dropdown-menu">
-                                                <li><a href="https://in.explara.com/e/laravelive-india-rajkot-meetup" target="_blank">Rajkot Meetup</a></li>
+                                                <li><a href="https://in.explara.com/e/laravelive-india-pune-meetup" target="_blank">Pune Meetup</a></li>
                                             </ul>
-                                        </li>                                        
+                                        </li>
                                         <li><a class="csi-scroll csi-btn" style="background-color: #f55247" href=" https://www.papercall.io/laravellive-india">Call For Papers</a></li>
                                     </ul>
                                 </div>
