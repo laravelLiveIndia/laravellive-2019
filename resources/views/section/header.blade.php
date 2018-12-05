203ebbef374b5b84b9b9b077416f223c4de5b238
sarthaksavvy <sarthak@bitfumes.com>
2018-12-05 10:33:44 +0530

    added mumbai meetup link

--- resources/views/section/header.blade.php
+++ resources/views/section/header.blade.php
@@ -42,8 +42,9 @@
                                                 class="dropdown-toggle" data-toggle="dropdown" role="button"
                                                 aria-haspopup="true" aria-expanded="false">Community <span class="caret"></span></a>
                                             <ul class="dropdown-menu">
-                                                <li><a href="https://in.explara.com/e/laravelive-india-pune-meetup" target="_blank">Pune Meetup</a></li>
-                                                <li><a href="https://in.explara.com/e/laravelive-india-ahmedabad-meetup" target="_blank">Ahmedabad Meetup</a></li>
+                                                <li><a href="https://in.explara.com/e/laravelive-india-mumbai-meetup" target="_blank">Mumbai Meetup</a></li>
+                                                <li><a href="https://in.explara.com/e/laravelive-india-ahmedabad-meetup" target="_blank"><s>Ahmedabad Meetup</s></a></li>
+                                                <li><a href="https://in.explara.com/e/laravelive-india-pune-meetup" target="_blank"><s>Pune Meetup</s></a></li>
                                             </ul>
                                         </li>
                                         <li><a class="csi-scroll csi-btn" style="background-color: #f55247" href=" https://www.papercall.io/laravellive-india">Call For Papers</a></li>
