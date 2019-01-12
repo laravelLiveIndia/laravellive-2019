142c3fbf57ab8ef450d682a4bfdcad4db69012c1
Abhishek Jain <jazeabby@gmail.com>
2019-01-12 15:33:50 +0530

    html and css changes for responsiveness

--- resources/views/section/header.blade.php
+++ resources/views/section/header.blade.php
@@ -28,32 +28,35 @@
                                     
                                 </div>
 
-                                {{--<div class="collapse navbar-collapse">--}}
-                                    {{--<ul class="nav navbar-nav csi-nav" id="mobile_li">--}}
-                                        {{--<li class="dropdown">--}}
-                                            {{--<a href="#">Community <span class="caret"></span></a>--}}
-                                            {{--<ul style="margin-left: -30px;">--}}
-                                                {{--<li><a href="https://allevents.in/ahmedabad/laravel-ahmedabad-meetup-december-2018-chapter/80002057491478" target="_blank">Ahmedabad Meetup</a></li>--}}
-                                            {{--</ul>--}}
-                                        {{--</li>--}}
-                                        {{--<li><a class="csi-scroll" href="https://www.papercall.io/laravellive-india">Call For Papers</a></li>--}}
-                                    {{--</ul>--}}
-                                    {{--<ul class="nav navbar-nav csi-nav" id="desktop_li">--}}
-                                        {{--<li class="dropdown">--}}
-                                            {{--<a href="http://httpcoder.com/demo/html/eventpro/view/home-typed.html#"--}}
-                                                {{--class="dropdown-toggle" data-toggle="dropdown" role="button"--}}
-                                                {{--aria-haspopup="true" aria-expanded="false">Community <span class="caret"></span></a>--}}
-                                            {{--<ul class="dropdown-menu">--}}
-                                                {{--<li><a href="https://allevents.in/ahmedabad/laravel-ahmedabad-meetup-december-2018-chapter/80002057491478" target="_blank">Ahmedabad Meetup</a></li>--}}
-                                            {{--</ul>--}}
-                                        {{--</li>--}}
-                                        {{--<li><a class="csi-scroll csi-btn" style="background-color: #f55247" href=" https://www.papercall.io/laravellive-india">Call For Papers</a></li>--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
+                                <div class="collapse navbar-collapse">
+                                    <ul class="nav navbar-nav csi-nav" id="mobile_li">
+                                        {{--<li class="dropdown">
+                                            <a href="#">Community <span class="caret"></span></a>
+                                            <ul style="margin-left: -30px;">
+                                                <li><a href="https://allevents.in/ahmedabad/laravel-ahmedabad-meetup-december-2018-chapter/80002057491478" target="_blank">Ahmedabad Meetup</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a class="csi-scroll" href="https://www.papercall.io/laravellive-india">Call For Papers</a></li> --}}
+                                        <li><button id=”open-popup” class="csi-btn" style="background-color: #f55247" onclick="showMailingPopUp()">Join Early Bird list</button></li>
+                                    </ul>
+                                    <ul class="nav navbar-nav csi-nav" id="desktop_li">
+                                        {{--<li class="dropdown">
+                                            <a href="http://httpcoder.com/demo/html/eventpro/view/home-typed.html#"
+                                                class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                                aria-haspopup="true" aria-expanded="false">Community <span class="caret"></span></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="https://allevents.in/ahmedabad/laravel-ahmedabad-meetup-december-2018-chapter/80002057491478" target="_blank">Ahmedabad Meetup</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a class="csi-scroll csi-btn" style="background-color: #f55247" href=" https://www.papercall.io/laravellive-india">Call For Papers</a></li> --}}
+                                        <li><button href="javascript:void(0)" id=”open-popup” class="csi-btn" style="background-color: #f55247" onclick="showMailingPopUp()">Join Early Bird list</button></li>
+
+                                    </ul>
+                                </div>
                                 <!--/.nav-collapse -->
                             </div>
 
-                            <button id=”open-popup” class="csi-btn pull-right" style="background-color: #f55247"onclick="showMailingPopUp()">Join Early Bird list</button>
+                            <!-- <button id=”open-popup” class="csi-btn pull-right" style="background-color: #f55247"onclick="showMailingPopUp()">Join Early Bird list</button> -->
                         </nav>
                     </div>
                 </div>
