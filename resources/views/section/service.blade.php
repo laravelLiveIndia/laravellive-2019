f505ba713cbf394a2df1215bf563b5bf3c7494b4
Shivani Sehdev <shivanisehdev@Shivanis-MacBook-Air.local>
2019-01-14 12:10:15 +0530

    Venue added

---
+++ resources/views/section/service.blade.php
@@ -0,0 +1,40 @@
+<section>
+  <div id="csi-services" class="csi-services">
+      <div class="csi-inner">
+          <div class="container">
+              <div class="row">
+                  <div class="col-xs-12 col-sm-4">
+                      <div class="csi-single-service">
+                          <span class="icon"><i class="fa fa-map-signs" aria-hidden="true"></i></span>
+                          <div class="text-area">
+                              <h2 class="title"><a href="#">Venue Location</a></h2>
+                              <p><strong>CSInfotech Ltd,</strong> Software technology park (12th floor), kawran bazar, dhaka, bangladesh</p>
+                              <a class="csi-btn-simple" href="#">Read More</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-xs-12 col-sm-4">
+                      <div class="csi-single-service">
+                          <span class="icon"><i class="fa fa-hotel" aria-hidden="true"></i></span>
+                          <div class="text-area">
+                              <h2 class="title"><a href="#">Hotel &amp; Restaurant</a></h2>
+                              <p>Etiam vel ante ac lacus vestibulum rutrum. Aliquam vehicula, massa in auctor dapibus commodo quis vehicula lacus. </p>
+                              <a class="csi-btn-simple" href="#">Read More</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-xs-12 col-sm-4">
+                      <div class="csi-single-service">
+                          <span class="icon"><i class="fa fa-plane" aria-hidden="true"></i></span>
+                          <div class="text-area">
+                              <h2 class="title"><a href="#">Transport Facilities</a></h2>
+                              <p>Etiam vel ante ac lacus vestibulum rutrum. Aliquam vehicula, massa in auctor dapibus commodo quis vehicula lacus. </p>
+                              <a class="csi-btn-simple" href="#">Read More</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div><!-- //.CONTAINER -->
+      </div><!-- //.INNER -->
+  </div>
+</section>
--- resources/views/section/venue.blade.php
+++ resources/views/section/venue.blade.php
@@ -1,40 +1,22 @@
-<section>
-        <div id="csi-services" class="csi-services">
-            <div class="csi-inner">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="csi-single-service">
-                                <span class="icon"><i class="fa fa-map-signs" aria-hidden="true"></i></span>
-                                <div class="text-area">
-                                    <h2 class="title"><a href="#">Venue Location</a></h2>
-                                    <p><strong>CSInfotech Ltd,</strong> Software technology park (12th floor), kawran bazar, dhaka, bangladesh</p>
-                                    <a class="csi-btn-simple" href="#">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="csi-single-service">
-                                <span class="icon"><i class="fa fa-hotel" aria-hidden="true"></i></span>
-                                <div class="text-area">
-                                    <h2 class="title"><a href="#">Hotel &amp; Restaurant</a></h2>
-                                    <p>Etiam vel ante ac lacus vestibulum rutrum. Aliquam vehicula, massa in auctor dapibus commodo quis vehicula lacus. </p>
-                                    <a class="csi-btn-simple" href="#">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-4">
-                            <div class="csi-single-service">
-                                <span class="icon"><i class="fa fa-plane" aria-hidden="true"></i></span>
-                                <div class="text-area">
-                                    <h2 class="title"><a href="#">Transport Facilities</a></h2>
-                                    <p>Etiam vel ante ac lacus vestibulum rutrum. Aliquam vehicula, massa in auctor dapibus commodo quis vehicula lacus. </p>
-                                    <a class="csi-btn-simple" href="#">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- //.CONTAINER -->
-            </div><!-- //.INNER -->
-        </div>
-    </section>
+<section class="text-center container">
+  <h3 style="color:#f55247">Venue</h3>
+  <h2>Copper Cabana</h2>
+  <p>Sakinaka, Andheri East, Mumbai-400072</p><br>
+  <div class="col-md-12">
+    <div class="col-md-6">
+      <img src="{{ asset('images/copper1.png') }}">
+    </div>
+
+    <div class="col-md-6">
+      <img src="{{ asset('images/copper2.png') }}">
+    </div>
+  </div>
+
+  <div class="col-md-12" style="padding:20px">
+    <p style="font-size:20px">If you are coming by metro then nearest metro station is Sakinaka Metro Station. Cabs are available from there to Copper Cabana and it takes around 20 minutes to reach in normal traffic.
+    </p><br>
+    <p style="font-size:20px">
+    For Adddress queries you can call to hotel directly - <a href="tel:+9170454 04094">+9170454 04094</a>
+    </p>
+  </div>
+</section>
