31cb818b132470a4641595c3110732be55c6df63
shivani <shivanisehdev86@gmail.com>
2019-01-16 20:09:02 +0530

    Layout improved venue down speaker up

--- resources/views/section/venue.blade.php
+++ resources/views/section/venue.blade.php
@@ -1,22 +1,24 @@
-<section class="text-center container">
-  <h3 style="color:#f55247">Venue</h3>
-  <h2>Copper Cabana</h2>
-  <p>Sakinaka, Andheri East, Mumbai-400072</p><br>
-  <div class="col-md-12">
-    <div class="col-md-6">
-      <img src="{{ asset('images/copper1.png') }}">
+<section class="text-center csi-about2">
+  <div class="csi-about container">
+    <h3 style="color:#f55247">Venue</h3>
+    <h2>Copper Cabana</h2>
+    <p>Sakinaka, Andheri East, Mumbai-400072</p><br>
+    <div class="col-md-12">
+      <div class="col-md-6">
+        <img src="{{ asset('images/copper1.png') }}">
+      </div>
+
+      <div class="col-md-6 ">
+        <img src="{{ asset('images/copper2.png') }}">
+      </div>
     </div>
 
-    <div class="col-md-6">
-      <img src="{{ asset('images/copper2.png') }}">
+    <div class="col-md-12" style="padding:20px">
+      <p style="font-size:20px">If you are coming by metro then nearest metro station is Sakinaka Metro Station. Cabs are available from there to Copper Cabana and it takes around 20 minutes to reach in normal traffic.
+      </p><br>
+      <p style="font-size:20px">
+      For Adddress queries you can call to hotel directly - <a href="tel:+9170454 04094">+9170454 04094</a>
+      </p>
     </div>
-  </div>
-
-  <div class="col-md-12" style="padding:20px">
-    <p style="font-size:20px">Nearest metro station is Sakinaka Metro Station. It takes around 20 minutes to reach by car in normal traffic.
-    </p><br>
-    <p style="font-size:20px">
-    For Adddress queries you can call to hotel directly - <a href="tel:+9170454 04094">+9170454 04094</a>
-    </p>
   </div>
 </section>
